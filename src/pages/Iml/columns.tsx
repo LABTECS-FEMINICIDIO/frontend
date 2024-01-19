0de373b3aa6a26ead4ebc1fb061a43616cfa1160
Leticia95 <leticia.alves95@hotmail.com>
2024-01-19 15:04:02 -0400

    feat: intergração tela iml

--- src/pages/Iml/columns.tsx
+++ src/pages/Iml/columns.tsx
@@ -4,31 +4,25 @@ export const columns: GridColDef[] = [
   {
     field: "dataEntrada",
     headerName: "Data de entrada",
-    width: 150,
   },
   {
-    field: "dataSaída",
-    headerName: "Data de Saída",
-    width: 150,
+    field: "horaEntrada",
+    headerName: "Hora de Entrada",
   },
   {
     field: "sexo",
     headerName: "Sexo",
-    width: 150,
   },
   {
     field: "idade",
     headerName: "Idade",
-    width: 150,
   },
   {
-    field: "bairroRemocao",
+    field: "bairroDaRemocao",
     headerName: "Bairro da remoção",
-    width: 150,
   },
   {
     field: "causaMorte",
     headerName: "Causa da morte",
-    width: 150,
   },
 ];
--- src/pages/Iml/index.tsx
+++ src/pages/Iml/index.tsx
@@ -2,32 +2,46 @@ import { Box, Button, Typography } from "@mui/material";
 import { TableGrid } from "../../components/TableGrid";
 import { title, toolbar1 } from "../../styles";
 import { columns } from "./columns";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { styled } from '@mui/material/styles'
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import { findManyImlData } from "../../service/iml";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
-export function Iml(){
-    return(
-        <>
-        <Box sx={toolbar1}>    
+export function Iml() {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    findManyImlData()
+      .then((res) => setRows(res.data))
+      .catch((error) => console.error("Erro ao obter dados:", error));
+  }, []);
+
+  return (
+    <>
+      <Box sx={toolbar1}>
         <Typography style={title}>Relatório IML</Typography>
-        <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
-      Importar arquivo
-      <VisuallyHiddenInput type="file" />
-    </Button>
+        <Button
+          component="label"
+          variant="contained"
+          startIcon={<CloudUploadIcon />}
+        >
+          Importar arquivo
+          <VisuallyHiddenInput type="file" />
+        </Button>
       </Box>
-        <TableGrid rows={[]} columns={columns}/>
-        </>
-    )
+      <TableGrid rows={rows} columns={columns} />
+    </>
+  );
 }
