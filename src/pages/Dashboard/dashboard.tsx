02e733d6873b5931e41c851db1bcc14670143bce
Leticia95 <leticia.alves95@hotmail.com>
2024-03-28 17:23:11 -0400

    refactor: dashboard

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -2,10 +2,28 @@ import { Box, CircularProgress } from "@mui/material";
 import { useState } from "react";
 
 export function Dashboard() {
-  const [loading, setLoading] = useState(true);
+  const [loadingMetabase, setLoadingMetabase] = useState(true);
+
+  // Função para lidar com o evento de carregamento do Metabase
+  const handleMetabaseLoad = () => {
+    // Quando o Metabase estiver carregado, definimos o estado de loading como falso
+    setLoadingMetabase(false);
+  };
 
   return (
     <>
+      {loadingMetabase && (
+        <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "70vh",
+        }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
       <Box sx={{ display: "flex", flexWrap: "wrap", mt: 1 }}>
         <iframe
           src="http://24.199.108.245:3000/public/dashboard/284ce180-8847-4d64-8e0e-8937f47e889c"
@@ -13,7 +31,8 @@ export function Dashboard() {
           width="1650"
           height="670"
           allowTransparency
-        ></iframe>
+          onLoad={handleMetabaseLoad}
+        />
       </Box>
     </>
   );
