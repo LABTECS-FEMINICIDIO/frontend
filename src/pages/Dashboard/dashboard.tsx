927202a0b465305ac24886274dac40975945f49b
Leticia95 <leticia.alves95@hotmail.com>
2024-04-01 15:13:28 -0400

    refactor: link do metabase

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -26,7 +26,7 @@ export function Dashboard() {
       )}
       <Box sx={{ display: "flex", flexWrap: "wrap", mt: 1 }}>
         <iframe
-          src="https://24.199.108.245:3000/public/dashboard/f9723e06-9c4d-401a-b6e9-fec9ab7c9ab3"
+          src="https://graph.monitorafeminicidio.com/public/dashboard/f9723e06-9c4d-401a-b6e9-fec9ab7c9ab3"
           frameBorder="0"
           width="1650"
           height="670"
