6dd783c00dccb95e3e7fa1503903bc4c2da0b262
Leticia95 <leticia.alves95@hotmail.com>
2024-04-11 09:50:03 -0400

    refactor: feriados

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -27,6 +27,7 @@ export function Dashboard() {
       <Box sx={{ display: "flex", flexWrap: "wrap", mt: 1 }}>
         <iframe
           src="https://graph.monitorafeminicidio.com/public/dashboard/7be29569-672f-4959-809e-612a67f86bcd"
+          title="Dashboards"
           frameBorder="0"
           width="1650"
           height="670"
